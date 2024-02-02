c1c47f9559e7dbfbd085cfec4ecc916ebbe91249
Mangukiyavruti <Mangukiyavruti1005@gmail.com>
2024-02-02 20:22:50 +0530

    agecalculator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Age_calculate from './Age_calculate';
+
+function App() {
+  return (
+    <>
+      <Age_calculate/>
+    </>
+  );
+}
+
+export default App;
